ca465ce8858dd906c3cc466dc20a2e9636653edf
AngelUndeveloped <angelyairmendezsandoval@gmail.com>
2024-11-11 21:12:04 -0600

    started implementing photopicker, will create a new branch soon

--- app/src/main/java/com/ayms/basicobjectdetection/GallerySelectionDisplayActivity.kt
+++ app/src/main/java/com/ayms/basicobjectdetection/GallerySelectionDisplayActivity.kt
@@ -1,4 +1,66 @@
 package com.ayms.basicobjectdetection
 
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import coil.compose.AsyncImage
+import com.ayms.basicobjectdetection.ui.theme.BasicObjectDetectionTheme
+
 class GallerySelectionDisplayActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicObjectDetectionTheme {
+                var selectedImageUri = remember {
+                    mutableStateOf<Uri?>(null)
+                }
+
+                val singlePhotoPickerLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityReultContracts.PickVisualMedia(),
+                    onResult = { uri -> selectedImageUri = uri }
+                )
+
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    item {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceAround
+                        ) {
+                            Button(onClick = {
+                                singlePhotoPickerLauncher.launch(
+                                    PickVisualMediaRequest(ImageOnly)
+                                )
+                            }) { Text(text = "Select Image from Gallery") }
+                        }
+                    }
+
+                    item {
+                        AsyncImage(
+                            model = selectedImageUri,
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxWidth(),
+                            contentScale = ContentScale.Crop
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/ayms/basicobjectdetection/MainActivity.kt
+++ app/src/main/java/com/ayms/basicobjectdetection/MainActivity.kt
@@ -1,23 +1,35 @@
 package com.ayms.basicobjectdetection
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia.ImageOnly
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.ayms.basicobjectdetection.ui.theme.BasicObjectDetectionTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,11 +38,34 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BasicObjectDetectionTheme {
+                val selectedImageUri = remember {
+                    mutableStateOf<Uri?>(null)
+                }
+
+                val singlePhotoPickerLauncher = rememberLauncherForActivityResult(
+                    contract = PickVisualMedia(),
+                    onResult = { uri ->
+                        if (uri != null) {
+                            Log.d("PhotoPicker", "Selected URI: $uri")
+                        } else {
+                            Log.d("PhotoPicker", "No media selected")
+                        }
+                    }
+                )
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CombinedTitleAndDetectionTypeSelection()
+                    CombinedTitleAndDetectionTypeSelection(singlePhotoPickerLauncher)
+                    item {
+                        AsyncImage(
+                            model = selectedImageUri,
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxWidth(),
+                            contentScale = ContentScale.Crop
+                        )
+                    }
                 }
             }
         }
@@ -48,13 +83,18 @@ fun Title(title: String, modifier: Modifier = Modifier) {
 @Composable
 fun DetectionTypeSelection(
     liveDetectionButtonText: String,
-    photoDetectionButtonText: String
+    photoDetectionButtonText: String,
+    singlePhotoPickerLauncher: ActivityResultLauncher<PickVisualMediaRequest>
 ) {
     Column {
         ElevatedButton(onClick = { /*TODO*/ }) {
             Text(liveDetectionButtonText)
         }
-        ElevatedButton(onClick = { /* TODO */ }) {
+        ElevatedButton(onClick = {
+            singlePhotoPickerLauncher.launch(
+                PickVisualMediaRequest(ImageOnly)
+            )
+        }) {
             Text(photoDetectionButtonText)
         }
     }
@@ -64,7 +104,9 @@ fun DetectionTypeSelection(
  * Composable that displays the title and the detection type selection buttons.
  */
 @Composable
-fun CombinedTitleAndDetectionTypeSelection() {
+fun CombinedTitleAndDetectionTypeSelection(
+    singlePhotoPickerLauncher: ActivityResultLauncher<PickVisualMediaRequest>
+) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
@@ -73,15 +115,22 @@ fun CombinedTitleAndDetectionTypeSelection() {
         Spacer(modifier = Modifier.height(16.dp))
         DetectionTypeSelection(
             liveDetectionButtonText = "Live Detection",
-            photoDetectionButtonText = "Photo Detection"
+            photoDetectionButtonText = "Photo Detection",
+            singlePhotoPickerLauncher = singlePhotoPickerLauncher
         )
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun TitlePreview() {
-    BasicObjectDetectionTheme {
-        CombinedTitleAndDetectionTypeSelection()
-    }
+fun item(function: @Composable () -> Unit) {
+
 }
+
+//@Preview(showBackground = true)
+//@Composable
+//fun TitlePreview() {
+//    BasicObjectDetectionTheme {
+//        CombinedTitleAndDetectionTypeSelection(
+//            singlePhotoPickerLauncher =
+//        )
+//    }
+//}
