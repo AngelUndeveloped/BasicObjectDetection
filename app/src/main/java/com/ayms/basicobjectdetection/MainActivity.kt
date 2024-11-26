ca88fab5531886b19d31413b5ac2cab231291e58
AngelUndeveloped <angelyairmendezsandoval@gmail.com>
2024-11-25 22:10:19 -0600

    implemented the photo picker. Now I just need to display the image.

--- app/src/main/java/com/ayms/basicobjectdetection/MainActivity.kt
+++ app/src/main/java/com/ayms/basicobjectdetection/MainActivity.kt
@@ -1,35 +1,25 @@
 package com.ayms.basicobjectdetection
 
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
-import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia.ImageOnly
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import com.ayms.basicobjectdetection.ui.theme.BasicObjectDetectionTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,7 +28,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BasicObjectDetectionTheme {
-
+                DetectionModeSelector()
             }
         }
     }
@@ -46,19 +36,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DetectionModeSelector() {
-    Column(modifier =
-    Modifier.fillMaxSize(),
+    val pickMedia =
+        rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                Log.d("PhotoPicker", "Selected URI: $uri.")
+            } else {
+                Log.d("PhotoPicker", "No media selected.")
+            }
+        }
+
+    Column(
+        modifier =
+        Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         ElevatedButton(onClick = { /*TODO*/ }) {
             Text(text = "Live Detection")
         }
         Spacer(modifier = Modifier.height(10.dp))
-        ElevatedButton(onClick = { /*TODO*/ }) {
-            Text(text = "Image Detection")
+        ElevatedButton(onClick = {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }) {
+            Text(text = "Image Selection")
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
